4d7cf464edc322d85bf806c76ac2f569ed968c03
yasimaru00 <yasimaru00@gmail.com>
2022-06-08 10:10:54 +0700

    fix conflix

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controller\{
-    UserController,
-};
+// use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,15 +15,32 @@ use App\Http\Controller\{
 |
 */
 
+// Route::get('/', function () {
+//     return Inertia::render('Welcome', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//     ]);
+// });
+
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return Inertia::render('Dashboard');
+//     })->name('dashboard');
+// });
 Route::get('/', function () {
     return redirect()->route('login');
 });
 
-Route::group(['middleware'=>'level:1'], function(){
-    Route::get('/user/data', [UserController::class, 'data'])->name('user.data');
-    Route::resource('/user', UserController::class);
-});
-Route::group(['middleware'=>'level:1,2'], function(){
-    Route::get('/profil', [UserController::class, 'profil'])->name('user.profil');
-    Route::resource('/profil', UserController::class);
-});
+// Route::group(['middleware' => 'level:1'], function () {
+    // Route::get('/user/data', [UserController::class, 'data'])->name('user.data');
+    // Route::resource('/user', UserController::class);
+// });
+
+    // Route::get('/profil', [UserController::class, 'profil'])->name('user.profil');
+    // Route::post('/profil', [UserController::class, 'updateProfil'])->name('user.update_profil');
