2f7e65cc12d16c108f803cfa5f2c0af4afedb522
yasimaru00 <yasimaru00@gmail.com>
2022-06-13 10:37:55 +0700

    perbaikan error dan bug view

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>{{ $setting->nama_perusahaan }} | @yield('title')</title>
+    <!-- <?php
+    //  dd($setting->nama_perusahaan); 
+     ?> -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
