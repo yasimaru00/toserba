159c45c1af6aa9e79bc1952710727cedaeef81bc
IrfanAliGit77 <raygensh77@gmail.com>
2022-06-14 18:53:20 +0700

    Tambah comment index  kategori

--- resources/views/kategori/index.blade.php
+++ resources/views/kategori/index.blade.php
@@ -1,9 +1,11 @@
 @extends('layouts.master')
 
+<!-- Judul url-->
 @section('title')
     Daftar Kategori
 @endsection
 
+<!-- Judul page-->
 @section('breadcrumb')
     @parent
     <li class="active">Daftar Kategori</li>
@@ -14,9 +16,11 @@
     <div class="col-lg-12">
         <div class="box">
             <div class="box-header with-border">
+                <!-- button penambahan kategori-->
                 <button onclick="addForm('{{ route('kategori.store') }}')" class="btn btn-success btn-xs btn-flat"><i class="fa fa-plus-circle"></i> Tambah</button>
             </div>
             <div class="box-body table-responsive">
+                <!-- Tabel isi dari data kategori-->
                 <table class="table table-stiped table-bordered">
                     <thead>
                         <th width="5%">No</th>
@@ -35,7 +39,7 @@
 @push('scripts')
 <script>
     let table;
-
+    // Sorting isi tabel
     $(function () {
         table = $('.table').DataTable({
             responsive: true,
@@ -51,7 +55,7 @@
                 {data: 'aksi', searchable: false, sortable: false},
             ]
         });
-
+        
         $('#modal-form').validator().on('submit', function (e) {
             if (! e.preventDefault()) {
                 $.post($('#modal-form form').attr('action'), $('#modal-form form').serialize())
@@ -66,7 +70,7 @@
             }
         });
     });
-
+    // fungsi penambahan data kategori
     function addForm(url) {
         $('#modal-form').modal('show');
         $('#modal-form .modal-title').text('Tambah Kategori');
@@ -76,7 +80,7 @@
         $('#modal-form [name=_method]').val('post');
         $('#modal-form [name=nama_kategori]').focus();
     }
-
+    // form edit data kategori 
     function editForm(url) {
         $('#modal-form').modal('show');
         $('#modal-form .modal-title').text('Edit Kategori');
@@ -95,7 +99,7 @@
                 return;
             });
     }
-
+    // form hapus data kategori
     function deleteData(url) {
         if (confirm('Yakin ingin menghapus data terpilih?')) {
             $.post(url, {
