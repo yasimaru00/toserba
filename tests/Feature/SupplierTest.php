f7bf805fbfc6950879ad8ae775e1fbf0765d4a48
IrfanAliGit77 <raygensh77@gmail.com>
2022-09-24 18:18:17 +0700

    restore fix bug testing supplier galiley

--- tests/Feature/SupplierTest.php
+++ tests/Feature/SupplierTest.php
@@ -16,4 +16,74 @@ use function PHPUnit\Framework\assertTrue;
 class SupplierTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function test_supplier_page()
+    {
+
+        $response = $this->get('/supplier');
+
+        $response->assertStatus(302);
+    }
+
+
+    public function test_supplier_store()
+    {
+        $supplier1 = Supplier::make([
+            'nama' => 'Jhonsonwqewq',
+            'alamat' => 'Malang',
+            'telepon' => '0987654342',
+        ]);
+        $supplier2 = Supplier::make([
+            'nama' => 'Badangqwewq',
+            'alamat' => 'Batu',
+            'telepon' => '09876543233',
+        ]);
+
+        $this->assertTrue($supplier1->nama != $supplier2->nama);
+
+        // $this->assertDatabaseHas('supplier', [
+        //     'nama' => 'Sukri',
+        //     'alamat' => 'Malang',
+        //     'telepon' => '0987654342',
+        // ]);
+    }
+
+    public function test_supplier_update()
+    {
+        $supplier = Supplier::where('id_supplier', 1)->update([
+            'nama' => 'Popoqweqw',
+            'alamat' => 'Nganjuk',
+            'telepon' => '3434323442',
+        ]);
+
+        $this->assertDatabaseMissing(
+            'supplier',
+            [
+
+                'nama' => 'Popoqweq',
+                'alamat' => 'Nganjuk',
+                'telepon' => '3434323442',
+            ]
+        );
+    }
+
+    public function test_supplier_delete()
+    {
+        $supplier = Supplier::first();
+        if ($supplier) {
+            $supplier->delete();
+        }
+        $this->assertTrue(true);
+    }
+
+    public function  test_it_store_new_supplier()
+    {
+        $response = $this->post('/supplier', [
+            'nama' => 'Badangqwe',
+            'alamat' => 'Batu3',
+            'telepon' => '098765432233',
+        ]);
+
+        $response->assertRedirect()->dump('/supplier');
+    }
 }
