8743efc14d6fff621202e728a015b1a22b85ef96
IrfanAliGit77 <raygensh77@gmail.com>
2022-09-24 18:53:16 +0700

    bug fixing

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -7,7 +7,7 @@
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-    <link rel="icon" href="{{ url($setting->path_logo) }}" type="image/png">
+    <link rel="icon" href="{{ url('/logo.png') }}" type="image/png">
     
 
     <!-- Bootstrap 3.3.7 -->
